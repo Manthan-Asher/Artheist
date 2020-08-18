795a161c27389038d33e6a3155d47009074b6924
Arpit Bhardwaj <f20180075@goa.bits-pilani.ac.in>
2020-08-18 10:42:04 +0530

    Fixed Homepage + Video React issue

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import {Provider} from "react-redux";
-import {createStore, applyMiddleware, compose} from "redux";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
 
 import App from "./components/App";
@@ -9,7 +9,9 @@ import "./index.css";
 import reducers from "./reducers";
 
 const composeEnhancers =
-  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({trace: true}) || compose;
+  (process.env.NODE_ENV === "development"
+    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+    : null) || compose;
 
 const store = createStore(reducers, composeEnhancers(applyMiddleware(thunk)));
 
